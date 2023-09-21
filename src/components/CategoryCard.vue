8c29f465438bfbbf36dccfa6765fca44ff9bf662
Zahra Mirali <z.mirali1997@gmail.com>
2023-09-21 12:16:48 +0200

    pass icon as slot to CategoryCard

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="card">
-        <span class="icon">
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.2" stroke-linecap="round"
-                 stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M14.5 3.5C14.5 3.5 14.5 5.5 12 5.5C9.5 5.5 9.5 3.5 9.5 3.5H7.5L4.20711 6.79289C3.81658 7.18342 3.81658 7.81658 4.20711 8.20711L6.5 10.5V20.5H17.5V10.5L19.7929 8.20711C20.1834 7.81658 20.1834 7.18342 19.7929 6.79289L16.5 3.5H14.5Z"/>
-            </svg>
-        </span>
+    <span class="icon">
+      <slot name="icon"></slot>
+    </span>
     <h4>{{ name }}</h4>
     <p>
       Standard chunk of Lorem Ipsum used since the 1500s is showed below
@@ -16,7 +12,7 @@
 </template>
 
 <script setup>
-import {defineProps} from 'vue';
+import { defineProps } from 'vue';
 
 const props = defineProps(['name']);
 
@@ -59,7 +55,7 @@ const props = defineProps(['name']);
   transition: background-color .25s, border-color .25s;
 }
 
-.card .icon svg {
+.card .icon :slotted(svg) {
   position: relative;
   z-index: 1;
   display: block;
@@ -99,7 +95,7 @@ const props = defineProps(['name']);
   border-color: rgba(52, 211, 153, 0.2);
 }
 
-.card:hover .icon svg {
+.card:hover .icon :slotted(svg) {
   color: #34D399;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,43 @@
 <template>
-    <h1>Welcome to the Search App</h1>
+    <h1>Welcome to Search App</h1>
     <p>Please use the search bar to find results.</p>
     <div class="grid">
-        <CategoryCard v-for="category of categories" :name="category" />
+        <CategoryCard v-for="category of categories" :name="capitalizeFirstLetter(category)">
+            <template v-slot:icon>
+                <component :is="getIconComponent(category)" />
+            </template>
+        </CategoryCard>
     </div>
 </template>
   
 <script setup>
 import { inject, } from 'vue';
 import CategoryCard from '../components/CategoryCard.vue';
+import IconPeople from '../components/icons/IconPeople.vue';
+import IconLocation from '../components/icons/IconLocation.vue';
+import IconCompany from '../components/icons/IconCompany.vue';
+import IconProduct from '../components/icons/IconProduct.vue';
 
 const categories = inject("categories");
+
+const getIconComponent = (type) => {
+    switch (type) {
+        case "people":
+            return IconPeople;
+        case "companies":
+            return IconCompany;
+        case "products":
+            return IconProduct;
+        case "locations":
+            return IconLocation;
+        default:
+            return 'div';
+    }
+};
+
+function capitalizeFirstLetter(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
 </script>
   
 <style scoped>
