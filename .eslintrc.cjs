87d2de94a88be1a47f0e6935f635a7f2e99c37bf
Zahra Mirali <z.mirali1997@gmail.com>
2023-09-22 23:41:14 +0200

    fix linter errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:jest/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  globals: {
+    module: 'readonly'
   }
 }
